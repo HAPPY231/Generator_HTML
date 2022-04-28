edeaa188b76ef146fc7611881e2908411f839fba
HAPPY231 <marszycki056@gmail.com>
2022-04-28 21:21:16 +0200

    Ostateczna wersja PHP

--- index.php
+++ index.php
@@ -10,46 +10,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="java.js"></script>
 </head>
-<body>
-    <div class="sematic">
+<body onload="generate()">
+    <center><h1>Generator HTML</h1></center>
+    <div class="two">
+    <div class="semantic">
     <?php 
 
     $select = "SELECT * FROM podstawowe";
     $res = mysqli_query($dbc,$select);
+
     while($row = mysqli_fetch_row($res)){
 
 echo<<<END
-    <input type="submit" value="$row[1]" id="$row[1]" onclick="sprawdz('{$row[2]}')">
+    <input type="submit" value="$row[1]" id="$row[1]" onclick="sprawdz('{$row[2]}')"> 
 END;
+
+
     }
+
 
     ?>
 
-<input type="submit" value="HTML" id="HTML" onclick="struktura_HTML()">
-<input type="submit" value="HTML" id="HTML" onclick="struktura_table()">
+    </div>
+    <div class="structur">
+        <input type="submit" value="Struktura HTML" id="HTML" onclick="struktura_HTML()">
+        <input type="submit" value="Tabela" id="HTML" onclick="struktura_table()">
+    </div>
 </div>
-<textarea id="text" style="width: 60vh; height: 80vh "></textarea>
+<div class="textareas">
+    <img src="recycle.png" alt="recycle.png" onclick="Cclear()" width="64" height="64" style="position: fixed; bottom: 100px; right: 50px;">
 
-<script>
-    function sprawdz(a){
-        var curPos = document.getElementById("text").selectionStart;
-        let x = $("#text").val();
-        let text_to_insert = a+"\n";
-        $("#text").val(
-x.slice(0, curPos) + text_to_insert + x.slice(curPos));
-    }
-    function struktura_HTML(){
-        var curPos = document.getElementById("text").selectionStart;
-        let x = $("#text").val();
-        let text_to_insert = "<!DOCTYPE html> \n<html lang='en'> \n<head> \n<meta charset='UTF-8'> \n<meta http-equiv='X-UA-Compatible' content='IE=edge'> \n<meta name='viewport' content='width=device-width, initial-scale=1.0'> \n<title>Document</title> \n</head> \n<body> \n \n</body> \n</html> \n ";
-        $("#text").val(
-x.slice(0, curPos) + text_to_insert + x.slice(curPos));
-    }
+    <textarea id="text" style="width: 60vh; height: 80vh"></textarea>
+    <textarea id="structures" style="width: 60vh; height: 80vh; transition: all 0.7s ease-in-out;"></textarea>
+</div>
+<footer>
+    Kacper Marszycki
+</footer>
 
-</script>
+
 </body>
 </html>
 
